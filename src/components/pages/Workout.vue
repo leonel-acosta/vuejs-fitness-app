6a68de970dfb66d8ea227ea8fe9517dd34bb2afb
leonel-acosta <leoacosta88@gmail.com>
2026-02-12 10:07:14 +0100

    modal layout and styling

--- src/components/pages/Workout.vue
+++ src/components/pages/Workout.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { workoutProgram } from "../../utils";
+import Portal from "../Portal.vue";
 const selectedWorkout = 4;
 const { workout, warmup } = workoutProgram[selectedWorkout];
 </script>
 
 <template>
+  <Portal>PUSHUPS</Portal>
   <section id="workout-card">
     <div class="plan-card card">
       <div class="plan-card-header">
